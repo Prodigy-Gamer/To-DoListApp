84251d2c7db5fa075b5ef06e417ead5000fa7e49
Yakshith Kommineni <kommineniaashith7@gmail.com>
2025-07-17 20:22:48 -0400

    I finished The To-Do List App!

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ def showmenu():
   print('4. Delete Task')
   print('5. Save & Exit')
 
-tasks = [{"task": "Complete Homework", "completed": False}]
+tasks = []
 
 def view_tasks():
   if not tasks:
@@ -27,3 +27,27 @@ def mark_complete():
   num = int(input('Enter your task: ')) - 1
   tasks[num]['completed'] = True
   print('Task Has Been Completed')
+
+def delete_task():
+  view_tasks()
+  num = int(input('Enter your task: ')) - 1
+  removed_task = tasks.pop(num)    
+  name = removed_task['task']
+  print(name + 'has been removed')      
+
+a = 2
+while a < 3:
+    showmenu()
+    num = int(input('Enter What You Would Like To Do: '))
+    if num == 1:
+      view_tasks()
+    if num == 2:
+      add_task()
+    if num == 3:
+      mark_complete()
+    if num == 4:
+      delete_task()
+    if num == 5:
+      print('goodbye')
+      break
+    
